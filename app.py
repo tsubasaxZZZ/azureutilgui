cc6e6d58d661a4bff8c2678fec6d29340988b8b1
Tsubasa Nomura <tsunomur@microsoft.com>
2017-10-03 01:58:02 +0900

    Referctaring code

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, redirect, url_for, session, request, jsonify, render_template
-from flask_oauthlib.client import OAuth, OAuthException, log
-from flask_sslify import SSLify
 import sys
 from pprint import pprint
 import base64
 import traceback
 import jwt
 import json
+from flask import Flask, redirect, url_for, session, request, render_template
+from flask_oauthlib.client import OAuth, OAuthException, log
+from flask_sslify import SSLify
+
 # from flask_sslify import SSLify
 
 import uuid
@@ -45,97 +46,97 @@ config.read('config')
 
 #---- for v1 endpoint(not supported now)
 microsoft = oauth.remote_app(
-	'microsoft',
-	consumer_key='5ca7d032-9ad5-46e1-9398-65242d4488c5',
-        consumer_secret=config.get('setting', 'CONSUMER_SECRET'),
-        base_url='https://management.azure.com/',
-	request_token_url=None,
-	access_token_method='POST',
-	access_token_url='https://login.microsoftonline.com/common/oauth2/token',
-	authorize_url='https://login.microsoftonline.com/common/oauth2/authorize?resource=https://management.azure.com/'
+    'microsoft',
+    consumer_key='5ca7d032-9ad5-46e1-9398-65242d4488c5',
+    consumer_secret=config.get('setting', 'CONSUMER_SECRET'),
+    base_url='https://management.azure.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://login.microsoftonline.com/common/oauth2/token',
+    authorize_url='https://login.microsoftonline.com/common/oauth2/authorize?resource=https://management.azure.com/'
 )
 
 
 @app.route('/')
 def index():
-	return render_template('hello.html')
+    return render_template('hello.html')
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
 
-	if 'microsoft_token' in session:
-		return redirect(url_for('subscriptions'))
+    if 'microsoft_token' in session:
+        return redirect(url_for('subscriptions'))
 
 	# Generate the guid to only accept initiated logins
-	guid = uuid.uuid4()
-	session['state'] = guid
+    guid = uuid.uuid4()
+    session['state'] = guid
 
-	return microsoft.authorize(callback=url_for('authorized', _external=True), state=guid)
-	
+    return microsoft.authorize(callback=url_for('authorized', _external=True), state=guid)
+
 @app.route('/logout', methods = ['POST', 'GET'])
 def logout():
-        session_clear()
+    session_clear()
 
-        logger.debug(config.get('setting', 'SITE_URL'))
-#        return redirect('https://login.microsoftonline.com/common/oauth2/logout')
-        return redirect('https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=' + str(config.get('setting', 'SITE_URL')))
+    logger.debug(config.get('setting', 'SITE_URL'))
+#    return redirect('https://login.microsoftonline.com/common/oauth2/logout')
+    return redirect('https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=' + str(config.get('setting', 'SITE_URL')))
 
 @app.route('/login/authorized')
 def authorized():
-        response = microsoft.authorized_response()
-        
-        if response is None:
-                return "Access Denied: Reason=%s\nError=%s" % (
-                        response.get('error'),
-                        request.get('error_description')
-                )
+    response = microsoft.authorized_response()
 
-	# Check response for state
-        logger.debug("Response: " + str(response))
-        if str(session['state']) != str(request.args['state']):
-                raise Exception('State has been messed with, end authentication')
+    if response is None:
+        return "Access Denied: Reason=%s\nError=%s" % (
+            response.get('error'),
+            request.get('error_description')
+        )
 
-	# Okay to store this in a local variable, encrypt if it's going to client
-        # machine or database. Treat as a password. 
-        session['microsoft_token'] = (response['access_token'], '')
-        access_token = session['microsoft_token'][0]
-        session['decoded_access_token'] = jwt.decode(access_token, verify=False)
+    # Check response for state
+    logger.debug("Response: " + str(response))
+    if str(session['state']) != str(request.args['state']):
+        raise Exception('State has been messed with, end authentication')
 
-        # ToDo : サブスクリプションがない場合の例外処理
-        try:
-                subscription_list = (microsoft.get('subscriptions?api-version=2014-04-01').data)['value']
-        except:
-                pass
-        else:
-                session['subscription_list'] = subscription_list
-                session['selected_subscription'] = subscription_list[0]
+    # Okay to store this in a local variable, encrypt if it's going to client
+    # machine or database. Treat as a password. 
+    session['microsoft_token'] = (response['access_token'], '')
+    access_token = session['microsoft_token'][0]
+    session['decoded_access_token'] = jwt.decode(access_token, verify=False)
 
-        logger.debug(session)
-        logger.debug(access_token)
-        logger.debug(session['decoded_access_token'])
+    # ToDo : サブスクリプションがない場合の例外処理
+    try:
+        subscription_list = (microsoft.get('subscriptions?api-version=2014-04-01').data)['value']
+    except:
+        pass
+    else:
+        session['subscription_list'] = subscription_list
+        session['selected_subscription'] = subscription_list[0]
 
-        return redirect(url_for('subscriptions', subscription_id=session['selected_subscription']['subscriptionId']))
+    logger.debug(session)
+    logger.debug(access_token)
+    logger.debug(session['decoded_access_token'])
+
+    return redirect(url_for('subscriptions', subscription_id=session['selected_subscription']['subscriptionId']))
 
 @app.route('/subscriptions/')
 @app.route('/subscriptions/<uuid:subscription_id>')
 def subscriptions(subscription_id=None):
-        try:
-                #me = microsoft.get('subscriptions?api-version=2014-04-01')
-                # サブスクリプションIDがパラメーターにない時はセッションから取得する
-                if subscription_id == None:
-                        return redirect(url_for('subscriptions', subscription_id=session['selected_subscription']['subscriptionId']))
+    try:
+        #me = microsoft.get('subscriptions?api-version=2014-04-01')
+        # サブスクリプションIDがパラメーターにない時はセッションから取得する
+        if subscription_id == None:
+            return redirect(url_for('subscriptions', subscription_id=session['selected_subscription']['subscriptionId']))
 
-                # パラメーターのサブスクリプションIDを、セッションに保存
-                session['selected_subscription'] = [s for s in session['subscription_list'] if s['subscriptionId'] == str(subscription_id)][0]
+        # パラメーターのサブスクリプションIDを、セッションに保存
+        session['selected_subscription'] = [s for s in session['subscription_list'] if s['subscriptionId'] == str(subscription_id)][0]
 
-                logger.debug("================")
-                logger.debug(session['selected_subscription'])
-                logger.debug(subscription_id)
-                logger.debug("================")
+        logger.debug("================")
+        logger.debug(session['selected_subscription'])
+        logger.debug(subscription_id)
+        logger.debug("================")
 
-                vm_list = microsoft.get('subscriptions/{}/providers/Microsoft.Compute/virtualMachines?api-version=2017-03-30'.format(subscription_id)).data
-                #logger.debug(vm_list)
-                vm_num = len(vm_list['value'])
+        vm_list = microsoft.get('subscriptions/{}/providers/Microsoft.Compute/virtualMachines?api-version=2017-03-30'.format(subscription_id)).data
+        #logger.debug(vm_list)
+        vm_num = len(vm_list['value'])
 
 #                vmnum = microsoft.get()
 #                from urllib2 import Request, urlopen, URLError
@@ -145,24 +146,24 @@ def subscriptions(subscription_id=None):
 #                                        None, headers)
 #                res = urlopen(req)
 #                me = res.read()
-                                                    
-        except:
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
-                logger.debug(''.join('!! ' + line for line in lines) )
-                session_clear()
-                return redirect(url_for('index')) 
-        else:
-                logger.debug(session['decoded_access_token'])
-                logger.debug(vm_list)
-                #logger.debug(microsoft.get('subscriptions?api-version=2014-04-01').data)
-                logger.debug(session['subscription_list'])
-                return render_template('dashboard.html', vm_num=vm_num)
+
+    except:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+        logger.debug(''.join('!! ' + line for line in lines))
+        session_clear()
+        return redirect(url_for('index')) 
+    else:
+        logger.debug(session['decoded_access_token'])
+        logger.debug(vm_list)
+        #logger.debug(microsoft.get('subscriptions?api-version=2014-04-01').data)
+        logger.debug(session['subscription_list'])
+        return render_template('dashboard.html', vm_num=vm_num)
 
 #@app.route('/subscriptions/')
 @app.route('/subscriptions/<uuid:subscription_id>/resourcegroup')
 def resourcegroup(subscription_id=None):
-        return render_template('resourcegroup.html')
+    return render_template('resourcegroup.html')
 
 
 # If library is having trouble with refresh, uncomment below and implement refresh handler
@@ -173,13 +174,13 @@ def resourcegroup(subscription_id=None):
 # def refresh():
 @microsoft.tokengetter
 def get_microsoft_oauth_token():
-	return session.get('microsoft_token')
+    return session.get('microsoft_token')
 
 def session_clear():
-        session.pop('microsoft_token', None)
-        session.pop('state', None)
-        session.pop('decoded_access_token', None)
+    session.pop('microsoft_token', None)
+    session.pop('state', None)
+    session.pop('decoded_access_token', None)
 
 
 if __name__ == '__main__':
-	app.run(port=10080, host='0.0.0.0')
+    app.run(port=10080, host='0.0.0.0')
